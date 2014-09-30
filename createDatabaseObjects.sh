fb41771cba5d3dc2e3d719b39c06774195b34f5e
Luis Mercado <lmercado@weblinc.com>
2014-09-30 19:15:20 -0400

    Scripts that create database files

---
+++ createDatabaseObjects.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+source "functions/createSprocs.sh"
+
+IFS=$'\n' read -d '' -r -a lines < inputFiles/variableList.txt
+
+#Up Migration
+filename='outputFiles/'${lines[0]}'_up.sql'
+filename=${filename//[[:space:]]/}
+
+numberOfLines=${#lines[@]}
+lastLine=`expr $numberOfLines - 1`
+
+echo "Creating... "$filename
+
+echo "SET ANSI_NULLS ON" > $filename
+echo "GO" >> $filename
+echo "SET QUOTED_IDENTIFIER ON" >> $filename
+echo "GO" >> $filename
+echo "SET ANSI_PADDING ON" >> $filename
+echo "GO" >> $filename
+echo "CREATE TABLE [dbo].["${lines[0]}"] (" >> $filename
+
+for (( i = 1 ; i < numberOfLines;  i++ ))
+do
+	#echo "    "${lines[$i]} >> $filename
+    if [ $i -eq $lastLine ]; then
+        echo "    @"${lines[$i]}"," >> $filename
+    else
+        echo "    @"${lines[$i]} >> $filename
+    fi
+done
+echo "CONSTRAINT [PK_"${lines[0]}"] PRIMARY KEY CLUSTERED" >> $filename
+echo "(" >> $filename
+        set -- ${lines[1]}
+        first=$1
+echo "    "$first" ASC" >> $filename
+echo ") WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON, FILLFACTOR = 90) ON [PRIMARY]
+) ON [PRIMARY]" >> $filename
+echo "GO" >> $filename
+
+
+declare -a arr=("GetDetail" "GetMultiple" "Insert" "Update" "Delete")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+    echo "" >> $filename
+    sproc=${lines[0]}${arr[@]}
+    sproc=${sproc//[[:space:]]/}
+    echo "CREATE PROCEDURE [dbo].["${sproc//[[:space:]]/}"]" >> $filename
+    echo "AS" >> $filename
+    echo "BEGIN EXEC('');" >> $filename
+    echo "END" >> $filename
+    echo "" >> $filename
+    echo "GO" >> $filename
+    echo "" >> $filename
+    # or do whatever with individual element of the array
+done
+
+
+#Down Migration
+filename='outputFiles/'${lines[0]}'_down.sql'
+filename=${filename//[[:space:]]/}
+echo "Creating... "$filename
+
+echo "DROP TABLE" ${lines[0]} > $filename
+echo "GO" >> $filename
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+    echo "" >> $filename
+    sproc=${lines[0]}${arr[@]}
+    sproc=${sproc//[[:space:]]/}
+    echo "IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].["${sproc//[[:space:]]/}"]') AND type in (N'P', N'PC'))" >> $filename
+    echo "DROP PROCEDURE [dbo].["${sproc//[[:space:]]/}"]" >> $filename
+    echo "" >> $filename
+    echo "GO" >> $filename
+    echo "" >> $filename
+    # or do whatever with individual element of the array
+done
+
+createSprocs
+
+
+	#echo -e "\t" $first " " $second
+	#echo "    "${lines[$i]} > $filename
+	#echo "${lines[$i]}"
+
+#printf "%s" "${lines[@]}"
+
+#echo "${lines}" | sed -e 's/^[ \t]*//'
---
+++ functions/createSprocs.sh
@@ -0,0 +1,166 @@
+function createSprocs {
+
+    #GetMultiple Sproc
+    filename='outputFiles/'${lines[0]}'GetMultiple.sql'
+    filename=${filename//[[:space:]]/}
+    echo "Creating... "$filename
+
+    echo "SET ANSI_NULLS ON" > $filename
+    echo "GO" >> $filename
+    echo "SET QUOTED_IDENTIFIER ON" >> $filename
+    echo "GO" >> $filename
+    sproc=${lines[0]}'GetMultiple'
+    sproc=${sproc//[[:space:]]/}
+    echo "ALTER PROCEDURE [dbo].["${sproc//[[:space:]]/}"]" >> $filename
+    echo "AS" >> $filename
+    echo "SELECT" >> $filename
+
+    lastLine=`expr $numberOfLines - 1`
+
+    for (( i = 1 ; i < numberOfLines; i++ ))
+    do
+        set -- ${lines[$i]}
+        first=$1
+        second=$2
+        if [ $i -eq $lastLine ]; then
+            echo "    "$first"," >> $filename
+        else
+            echo "    "$first >> $filename
+        fi
+    done
+
+    echo "FROM" >> $filename
+    echo "    dbo."${lines[0]}" WITH (NOLOCK)" >> $filename
+    echo "" >> $filename
+    echo "GO" >> $filename
+
+    #GetDetail Sproc
+    filename='outputFiles/'${lines[0]}'GetDetail.sql'
+    filename=${filename//[[:space:]]/}
+    echo "Creating... "$filename
+
+    echo "SET ANSI_NULLS ON" > $filename
+    echo "GO" >> $filename
+    echo "SET QUOTED_IDENTIFIER ON" >> $filename
+    echo "GO" >> $filename
+    sproc=${lines[0]}'GetDetail'
+    sproc=${sproc//[[:space:]]/}
+    echo "ALTER PROCEDURE [dbo].["${sproc//[[:space:]]/}"] (" >> $filename
+    echo "    @"${lines[1]//,/} >> $filename
+    echo ")" >> $filename
+    echo "AS" >> $filename
+    echo "SELECT" >> $filename
+
+    for (( i = 1 ; i < numberOfLines; i++ ))
+    do
+        set -- ${lines[$i]}
+        first=$1
+        second=$2
+        if [ $i -eq $lastLine ]; then
+            echo "    "$first >> $filename
+        else
+            echo "    "$first"," >> $filename
+        fi
+    done
+
+    echo "FROM" >> $filename
+    echo "    dbo."${lines[0]}" WITH (NOLOCK)" >> $filename
+    echo "WHERE" >> $filename
+    set -- ${lines[1]}
+    first=$1
+    echo "    "$first" = @"$first >> $filename
+    echo "" >> $filename
+    echo "GO" >> $filename
+
+
+    #Insert Sproc
+    filename='outputFiles/'${lines[0]}'Insert.sql'
+    filename=${filename//[[:space:]]/}
+    echo "Creating... "$filename
+
+    echo "SET ANSI_NULLS ON" > $filename
+    echo "GO" >> $filename
+    echo "SET QUOTED_IDENTIFIER ON" >> $filename
+    echo "GO" >> $filename
+    sproc=${lines[0]}'Insert'
+    sproc=${sproc//[[:space:]]/}
+    echo "ALTER PROCEDURE [dbo].["${sproc//[[:space:]]/}"] (" >> $filename
+    for (( i = 2 ; i < numberOfLines; i++ ))
+    do
+        if [ $i -eq $lastLine ]; then
+            echo "    @"${lines[$i]//,/} >> $filename
+        else
+            echo "    @"${lines[$i]} >> $filename
+        fi
+    done
+    echo ")" >> $filename
+    echo "AS" >> $filename
+    echo "INSERT INTO "${lines[0]} >> $filename
+    echo "(" >> $filename
+    for (( i = 1 ; i < numberOfLines; i++ ))
+    do
+        set -- ${lines[$i]}
+        first=$1
+        second=$2
+        if [ $i -eq $lastLine ]; then
+            echo "    "$first >> $filename
+        else
+            echo "    "$first"," >> $filename
+        fi
+    done
+    echo ")" >> $filename
+    echo "SELECT" >> $filename
+    for (( i = 1 ; i < numberOfLines; i++ ))
+    do
+        set -- ${lines[$i]}
+        first=$1
+        second=$2
+        if [ $i -eq $lastLine ]; then
+            echo "    @"$first >> $filename
+        else
+            echo "    @"$first"," >> $filename
+        fi
+    done
+    echo "" >> $filename
+    echo "GO" >> $filename
+
+
+    #Update Sproc
+    filename='outputFiles/'${lines[0]}'Update.sql'
+    filename=${filename//[[:space:]]/}
+    echo "Creating... "$filename
+
+    echo "SET ANSI_NULLS ON" > $filename
+    echo "GO" >> $filename
+    echo "SET QUOTED_IDENTIFIER ON" >> $filename
+    echo "GO" >> $filename
+    sproc=${lines[0]}'Update'
+    sproc=${sproc//[[:space:]]/}
+    echo "ALTER PROCEDURE [dbo].["${sproc//[[:space:]]/}"] (" >> $filename
+    for (( i = 1 ; i < numberOfLines; i++ ))
+    do
+        if [ $i -eq $lastLine ]; then
+            echo "    @"${lines[$i]//,/} >> $filename
+        else
+            echo "    @"${lines[$i]} >> $filename
+        fi
+    done
+    echo ")" >> $filename
+    echo "AS" >> $filename
+    echo "UPDATE "${lines[0]} >> $filename
+    echo "SET" >> $filename
+    for (( i = 1 ; i < numberOfLines; i++ ))
+    do
+        set -- ${lines[$i]}
+        first=$1
+        second=$2
+        if [ $i -eq $lastLine ]; then
+            echo "    "$first" = @"$first >> $filename
+        else
+            echo "    "$first" = @"$first"," >> $filename
+        fi
+    done
+    echo "" >> $filename
+    echo "GO" >> $filename
+
+}
